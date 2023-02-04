fb64a612c2852d0df1432f3dd8296b11e907f695
Tin Svagelj <tin.svagelj@live.com>
2023-02-05 00:52:40 +0100

    Add Python section
    Signed-off-by: Tin Svagelj <tin.svagelj@live.com>

--- code/der_f_x.py
+++ code/der_f_x.py
@@ -5,11 +5,14 @@ from mpl_toolkits.mplot3d import Axes3D
 x = np.linspace(-10, 10, 100)
 y = np.linspace(-10, 10, 100)
 X, Y = np.meshgrid(x, y)
-Z = 2*X**3 + 2*Y**3 - 36*X*Y + 430
-print(Z)
+Z = 6 * X**2 - 36 * Y
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.plot_surface(X, Y, Z)
 
-plt.savefig("figures/graf_fje.png")
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+#plt.savefig("figures/der_f_x.png")
