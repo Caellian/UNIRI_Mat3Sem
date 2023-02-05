f6acd1ff13868900966e1a55ec7776a889b22d27
Tin Svagelj <tin.svagelj@live.com>
2023-02-05 02:56:53 +0100

    Add page breaks in body
    Signed-off-by: Tin Svagelj <tin.svagelj@live.com>

--- code/graph.py
+++ code/graph.py
@@ -31,5 +31,5 @@ def save_figure(of, path, z_label='z', font_size='medium', angle=[-60, 30]):
     plt.savefig(path)
 
 save_figure(f(), 'figures/graf_f.png', z_label='$f(x, y)$', angle=[-70, 30])
-save_figure(f_der_x(), 'figures/graf_fdx.png', z_label='$\\frac{\\partial f}{\\partial x}$', font_size='x-large', angle=[60, 30])
-save_figure(f_der_y(), 'figures/graf_fdy.png', z_label='$\\frac{\\partial f}{\\partial y}$', font_size='x-large', angle=[60, 30])
+save_figure(f_der_x(), 'figures/graf_fdx.png', z_label='$\\frac{\\partial f}{\\partial x}$', font_size='xx-large', angle=[60, 30])
+save_figure(f_der_y(), 'figures/graf_fdy.png', z_label='$\\frac{\\partial f}{\\partial y}$', font_size='xx-large', angle=[60, 30])
